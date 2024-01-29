b7877832c776ce5052496bc9598cfac281c79722
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-01-29 06:43:39 -0300

    Add input parsing to Retrieval exercise

--- retrieval/script.js
+++ retrieval/script.js
@@ -1,5 +1,30 @@
+class InputParser {
+    constructor(input) {
+        this.rawInput = input;
+    }
+
+    getParsedInput() {
+        var parsedInputArray = [];
+
+        for (let i = 0; i < this.rawInput.length-1; i += 2) {
+            if (isNaN(this.rawInput[i])) {
+                continue;
+            }
+
+            parsedInputArray.push([this.rawInput[i], this.rawInput[i+1]]);
+        }
+
+        return parsedInputArray;
+    }
+}
+
 
 
 function runTest() {
-    console.log("Test");
+    var input = "1\n0\n7\n1 2 3 4 5 6 7\n3\n5 20 35";
+    var lines = input.split('\n');
+
+    const inputParser = new InputParser(lines);
+
+    console.log(inputParser.getParsedInput());
 }
